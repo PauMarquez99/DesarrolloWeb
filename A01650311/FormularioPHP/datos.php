230ebc451d274aafc2bb7f4c395ce5561101fb91
PauMarquez99 <pauilimq99@gmail.com>
2022-10-03 15:04:49 -0500

    Ejercicio PHP

---
+++ A01650311/FormularioPHP/datos.php
@@ -0,0 +1,9 @@
+<html>
+    <body>
+        <h1>Datos </h1>
+        Nombre: <?php echo $_POST["name"]; ?><br>
+        Telefono: <?php echo $_POST["tel"]; ?><br>
+        Correo: <?php echo $_POST["email"]; ?><br>
+        Mensaje: <?php echo $_POST["msg"]; ?><br>
+    </body>
+</html>
